c801be11e5dc519be93fcb104bbdb2585c43ffbc
Matt White <matt@ldcvia.com>
2015-07-01 13:50:26 +0100

    Add simple Domino REST integration

--- demo-app/routes/index.js
+++ demo-app/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var rest = require('restler'); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/domino', function(req, res, next) {
+
+  rest.get('http://dev.londc.com/demos/discussion.nsf/api/data/collections/unid/8178B1C14B1E9B6B8525624F0062FE9F')
+  .on('complete', function(data, response){ 
+    res.render('domino', {title: 'Domino Data', data: data});
+  });
+})
+
+router.get('/domino/:unid', function(req, res, next){
+  rest.get('http://dev.londc.com/demos/discussion.nsf/api/data/documents/unid/' + req.params.unid)
+  .on('complete', function(data, response){ 
+    res.render('dominodoc', {title: 'Domino Document', data: data});
+  });
+})
 
 module.exports = router;
